71d82f5836a105276b6d0df8c758932a8242bec3
Joe Butler <joefish.b@gmail.com>
2020-05-31 16:52:58 +1200

    Tiny refactor just moving lines around

--- game.js
+++ game.js
@@ -24,42 +24,42 @@ function scoreFrame(frame, nextFrame, thirdFrame) {
   let nextBallOne = null
   let nextBallTwo = null
 
-  let thirdBallOne = null
-  let thirdBallTwo = null
-
   if (nextFrame) {
     nextBallOne = nextFrame[0]
     nextBallTwo = nextFrame[1]
   }
+
+  let thirdBallOne = null
+  let thirdBallTwo = null
 
   if (thirdFrame) {
     thirdBallOne = thirdFrame[0]
     thirdBallTwo = thirdFrame[1]
   }
 
-  frameScore += isStandardFrame(frame, ballOne, ballTwo);
-  frameScore += isSpare(frame, ballOne, ballTwo, nextBallOne);
-  frameScore += isSingleStrike(frame, ballOne, nextBallOne, nextBallTwo);
-  frameScore += isDoubleStrike(frame, ballOne, nextBallOne, nextBallTwo, thirdBallOne);
-  frameScore += isLastFrame(frame, ballOne, ballTwo, ballThree);
+  frameScore += isStandardFrame(frame, ballOne, ballTwo)
+  frameScore += isSpare(frame, ballOne, ballTwo, nextBallOne)
+  frameScore += isSingleStrike(frame, ballOne, nextBallOne, nextBallTwo)
+  frameScore += isDoubleStrike(frame, ballOne, nextBallOne, nextBallTwo, thirdBallOne)
+  frameScore += isLastFrame(frame, ballOne, ballTwo, ballThree)
 
   return frameScore
 }
 
 function isStandardFrame(frame, ballOne, ballTwo) {
   if (frame.length != 3 && ballOne + ballTwo != 10) {
-    return ballOne + ballTwo;
+    return ballOne + ballTwo
   } else {
-    return 0;
+    return 0
   }
 }
 
 
 function isSpare(frame, ballOne, ballTwo, nextBallOne) {
   if (frame.length != 3 && ballOne + ballTwo == 10 && ballOne != 10) {
-    return ballOne + ballTwo + nextBallOne;
+    return ballOne + ballTwo + nextBallOne
   } else {
-    return 0;
+    return 0
   }
 }
 
@@ -73,9 +73,9 @@ function isSingleStrike(frame, ballOne, nextBallOne, nextBallTwo) {
 
 function isDoubleStrike(frame, ballOne, nextBallOne, nextBallTwo, thirdBallOne) {
   if (frame.length != 3 && ballOne == 10 && nextBallOne == 10 && thirdBallOne != null) {
-    return ballOne + nextBallOne + thirdBallOne;
+    return ballOne + nextBallOne + thirdBallOne
   } else if (frame.length != 3 && ballOne == 10 && nextBallOne == 10 && thirdBallOne == null) {
-    return ballOne + nextBallOne + nextBallTwo;
+    return ballOne + nextBallOne + nextBallTwo
   } else {
     return 0;
   }
@@ -83,8 +83,8 @@ function isDoubleStrike(frame, ballOne, nextBallOne, nextBallTwo, thirdBallOne) 
 
 function isLastFrame(frame, ballOne, ballTwo, ballThree) {
   if (frame.length == 3) {
-    return ballOne + ballTwo + ballThree;
+    return ballOne + ballTwo + ballThree
   } else {
-    return 0;
+    return 0
   }
 }
